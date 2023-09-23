396598a243ba36331599a5a72a047dbcb5217498
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2023-09-23 16:45:14 +0530

    Added datatype

---
+++ 01_Basic/datatype.js
@@ -0,0 +1,16 @@
+"use strict"; // for run in new update
+// tc39.es is the original documentationof js
+
+// Number => 2^53
+// BigInt => n
+// String
+// Boolean=> true false
+// null => standalone value
+// undefined
+// Symbol => unique
+
+// object/
+
+console.log(typeof null);  // object
+
+console.log(typeof undefined); // undefined
