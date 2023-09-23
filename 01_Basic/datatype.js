07f3df6e1450f2fab46b73a8df99079f987e0591
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2023-09-23 20:17:41 +0530

    non primitive data type

--- 01_Basic/datatype.js
+++ 01_Basic/datatype.js
@@ -1,6 +1,7 @@
 "use strict"; // for run in new update
 // tc39.es is the original documentationof js
 
+//// 7 types : (primitive)
 // Number => 2^53
 // BigInt => n
 // String
@@ -9,8 +10,12 @@
 // undefined
 // Symbol => unique
 
-// object/
+////Non primitive or reference
+// object
+// array
+// function
 
-console.log(typeof null);  // object
+// console.log(typeof null);  // object
 
-console.log(typeof undefined); // undefined
+// console.log(typeof undefined); // undefined
+
