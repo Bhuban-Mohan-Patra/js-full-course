5e04ebfffdcf8d11ce57504ee68328cab4d7f8f4
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2023-09-23 20:06:25 +0530

    some important comparision

---
+++ 01_Basic/comparision.js
@@ -0,0 +1,11 @@
+
+
+// console.log(null>0);  //false
+// console.log(null==0); //false
+// console.log(null===0); //false
+// console.log(null<=0); //true
+// console.log(null>=0); //true   
+
+// in js == or ===  the null is null  but during >= or <= or > or < the value is convert to number and value is 0 
+
+// equal and comparison has differentn working
