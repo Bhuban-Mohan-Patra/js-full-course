4146c0e3c291321222ae8e9ee887881f83a055d2
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2023-09-23 19:11:35 +0530

    some imp operations

---
+++ 01_Basic/operation.js
@@ -0,0 +1,13 @@
+
+// string and number
+
+// console.log(2+2);  //4
+// console.log(2+"a"); //2a
+// console.log("d"+2);  //d2
+// console.log(2+2+"b");  //4b  string
+// console.log(2+"b"+2); //2b2  string
+// console.log("b"+2+3); //b23   string
+
+// console.log(true); //true
+// console.log(+true); //1
+// console.log(+""); //0
