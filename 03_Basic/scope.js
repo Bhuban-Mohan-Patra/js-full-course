d41ae66a24fce6957f4e8f4bc071c480fbff234c
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2023-09-26 19:39:02 +0530

    scope and hoisting

---
+++ 03_Basic/scope.js
@@ -0,0 +1,40 @@
+// function one(){
+//     const username = "Bhuban";
+//     function two(){
+//         const website = "youtube";
+//         console.log(username);
+//         console.log(website);
+//     }
+//     two();
+//     // console.log(website);
+//     console.log(username);
+    
+// }
+// one();
+
+//============================================================================================
+
+// function addone(num){
+//     return num+1;
+// }
+
+// console.log(addone(5));
+
+
+// const addtwo = function (num){
+//     return num+2;
+// }
+
+// console.log(addtwo(5));
+
+//but when this type
+// console.log(addone(5));
+// function addone(num){
+//     return num+1;
+// }
+//======================  Hoisting ====================================================
+// console.log(addtwo(5));        // can not access the fuction addtwo   this is called  hoisting
+// const addtwo = function (num){
+//     return num+2;
+// }
+
