6a3ef7222df7ff2db67166e6413bfe9b6bebd098
Bhuban Mohan Patra <patrabhubanmohan@gmail.com>
2023-12-17 11:02:25 +0530

    change in array and filter .js

--- 02_Basic/array.js
+++ 02_Basic/array.js
@@ -1,8 +1,13 @@
 // declaration  ===========================================
 
-const myArr = [0,1,2,3,4,"hi",true];
+const myArr = [0,1,2,3,4,"hi",true];   // array literal method
 
 const myArr2 = new Array(1,2,3,4,5);
+
+const myArr3 = []
+myArr3[0]= "hello"
+myArr3[1]= "hello"
+myArr3[2]= "hii"
 
 //Access by indexing
 
@@ -10,26 +15,70 @@ const myArr2 = new Array(1,2,3,4,5);
 
 // Deep copy is the opposite of shallow copy
 
+// for each loop =================================================
+// myArr.forEach((item,index)=>{
+//     console.log(item);
+// })
+    // we can't use break in it  and every time it call a function
+
+// for in and for of loop =================================================
+// for(let i in myArr)
+// {
+//     console.log(i);
+// }
+    // it returns index beacause key in object
+
+// for(let i of myArr)
+// {
+//     console.log(i);
+// }
+
+
+
 
 //methods ================================================
 
 // console.log(myArr.length);
 
+// console.log(myArr.toString());  
+    // to string method converts an array to a string of comma separated vlaues
+
+
 // myArr.push(78);
 // myArr.pop();
+    // therer is also delete method but that leaves undefined holes in array so better to use pop and shift
+// delete myArr[0]
 // console.log(myArr);
 
-// myArr.unshift(-7);
+// myArr.unshift(-7);     
+// add at start
 // myArr.shift();
+// remove from start
 // console.log(myArr);
 
-// console.log(myArr.includes(87));  //returns boolean
-// console.log(myArr.indexOf(7));
 
+// search element in array =============================================
+    // for string search .search()
+// console.log(myArr.includes(87)); 
+    //returns boolean
+// console.log(myArr.indexOf(7));
+// console.log(myArr.lastIndexOf(7));
+    //(value , starting index)
+// console.log(myArr.find((value)=>{ value > 1}));
+    // return undefined if no value and only return one element
+// console.log(myArr.findIndex((value)=>{ value == 1}))
+
+
+// merging 2 array ====================================
 // const newArr = myArr.join();
 // console.log(myArr);
 // console.log(newArr);
 // console.log(typeof newArr);  //string datatype
+
+// const concatArray = myArr.concat(myArr2)
+    // returns a new arry and not change in existing array
+// console.log(concatArray);
+
 
 //slice -----------------------------------------
 // not change in original array
@@ -42,10 +91,21 @@ const myArr2 = new Array(1,2,3,4,5);
 // do change in array 
 // console.log("A ",myArr);
 // const mna2 = myArr.splice(1,3);  //including end index
-// console.log(mna2);     //datatype is arr ay
+    // (start position , no of elements)
+// console.log(mna2);     //datatype is array
 // console.log("B ",myArr);
 
- 
+// min max sort ===============================================
+// myArr.sort();
+// myArr.reverse();
+// console.log(myArr);
+
+
+ // flattening and array ===============================================
+    // reducing the dimension
+// const a = [[1,2],[3,4],[5,6]];
+// const b = a.flat();
+// console.log(b);
 
 // array as a element in another array ===========================
 
@@ -96,3 +156,14 @@ const myArr2 = new Array(1,2,3,4,5);
 // console.log(Array.of(score1,score2,score3,score4,score5));.
 //returns a new array
 
+
+
+
+// string methods =================================================
+
+// .search()
+// .trim()
+    //to remove whitespace
+// .split(',') 
+// .split('|')
+    //string to array
--- 05_loop and map/filter.js
+++ 05_loop and map/filter.js
@@ -1,5 +1,5 @@
 const array = [1,2,3,4,5,6,7,8,9,10]
-
+    // we use filter because foreach loop don't return anything    filter is of true and false
 let result = array.filter((item)=> item>3)  // return an array
 // console.log(result);
 
@@ -22,3 +22,13 @@ let answer = array.filter((item)=> {
 
 
 //================================== reduce ===============================================
+
+
+const myArray = [1,2,3,4,5,6,7,8]
+const initialValue = 0;
+const total = myArray.reduce((accumulator, currentValue)=>{
+    console.log(`accumulator: ${accumulator} , currentValue: ${currentValue}`);
+    return accumulator + currentValue
+},initialValue)
+
+console.log(total) // console;
